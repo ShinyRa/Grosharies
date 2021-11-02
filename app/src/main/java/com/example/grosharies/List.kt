f7ac0676bcc260f2dec53fbb90ea95ee2986cb65
mikal <mikal@machinenetwork.io>
2021-11-02 15:26:22 +0100

    added some database preparations

--- app/src/main/java/com/example/grosharies/List.kt
+++ app/src/main/java/com/example/grosharies/List.kt
@@ -1,9 +1,23 @@
 package com.example.grosharies
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "list")
 data class List(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long? = null,
+
+    @ColumnInfo(name = "listName")
     val listName: String,
+    @ColumnInfo(name = "lastEdited")
     val lastEdited: Long,
+    @ColumnInfo(name = "createdBy")
     val createdBy: String,
+    @ColumnInfo(name = "listItems")
     val listItems: ArrayList<ListItems>,
 ) {
 }
--- app/src/main/java/com/example/grosharies/ListItems.kt
+++ app/src/main/java/com/example/grosharies/ListItems.kt
@@ -1,8 +1,21 @@
 package com.example.grosharies
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "listItem")
 data class ListItems(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long? = null,
+
+    @ColumnInfo(name = "itemName")
     val itemName: String,
+    @ColumnInfo(name = "itemAmount")
     val itemAmount: Int,
+    @ColumnInfo(name = "itemPurchased")
     val itemPurchased: Boolean,
 ) {
 }
