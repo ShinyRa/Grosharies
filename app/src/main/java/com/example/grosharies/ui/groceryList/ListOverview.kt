7b1c1b309640d9299f198ae4c73f892588f153af
mikal <mikal@machinenetwork.io>
2021-11-10 14:22:25 +0100

    started the crud of the lists and added the edit page

--- app/src/main/java/com/example/grosharies/ui/groceryList/ListOverview.kt
+++ app/src/main/java/com/example/grosharies/ui/groceryList/ListOverview.kt
@@ -6,93 +6,126 @@ import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.grosharies.data.ListItem.ListItem
+import com.example.grosharies.ui.groups.Group
+import com.example.grosharies.ui.theme.GroshariesTheme
 
 @Composable
-fun CreateListOverview(
-    groupId: String? = null,
-    modifier: Modifier = Modifier
-) {
-    val groceryList: List<GroceryList> = getExampleData(groupId)
-    Box(modifier = modifier) {
-        LazyColumn(
-            contentPadding = PaddingValues(
-                start = 16.dp,
-                top = 8.dp,
-                end = 16.dp,
-                bottom = 8.dp
+fun ListOverview(groupId: String? = null, modifier: Modifier = Modifier) {
+    val (lists, setLists) = remember { mutableStateOf(listOf<GroceryList>()) }
+    val counter = remember { mutableStateOf(0) }
+
+    fun addGroceryList() {
+        counter.value = counter.value + 1
+        setLists(
+            lists + GroceryList(
+                "List ${counter.value}",
+                counter.value.toLong(),
+                "Name ${counter.value}",
+                listOf(
+                    ListItem("itemName ${counter.value}", counter.value, false),
+                    ListItem("itemName ${counter.value}", counter.value, false),
+                    ListItem("itemName ${counter.value}", counter.value, false)
+                )
             )
-        ) {
-            items(groceryList.size) { index ->
-                Card(
-                    backgroundColor = Color.White,
-                    modifier = Modifier.padding(PaddingValues(bottom = 16.dp))
+        )
+    }
+
+    fun removeFromLists(list: GroceryList) = setLists(lists.filter { it.id != list.id })
+
+    GroshariesTheme {
+        val groceryList: List<GroceryList> = getExampleData(groupId)
+        Box(modifier = modifier) {
+            Column() {
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        top = 8.dp,
+                        end = 16.dp,
+                        bottom = 8.dp
+                    )
                 ) {
-                    Column{
-                        Row(
-                            Modifier
-                                .padding(
-                                    PaddingValues(
-                                        start = 16.dp,
-                                        top = 8.dp,
-                                        end = 16.dp,
-                                    )
-                                )
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
+                    items(lists.size) { index ->
+                        Card(
+                            backgroundColor = Color.White,
+                            modifier = Modifier.padding(PaddingValues(bottom = 16.dp))
                         ) {
-                            Column(
-                                Modifier
-                                    .padding(8.dp)
-                            ) {
-                                Text(text = groceryList[index].listName)
-                                Text(text = "By: ${groceryList[index].createdBy}")
-                                Text(text = "Last edited: ${groceryList[index].lastEdited}")
-                            }
-                            Column(
-                                Modifier
-                                    .padding(8.dp),
-                                horizontalAlignment = Alignment.End
-                            ) {
-                                groceryList[index].listItems.take(2).forEach { item ->
-                                    Text(text = item.itemName, color = Color.Gray)
+                            Column {
+                                Row(
+                                    Modifier
+                                        .padding(
+                                            PaddingValues(
+                                                start = 16.dp,
+                                                top = 8.dp,
+                                                end = 16.dp,
+                                            )
+                                        )
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                ) {
+                                    Column(
+                                        Modifier
+                                            .padding(8.dp)
+                                    ) {
+                                        Text(text = lists[index].listName)
+                                        Text(text = "By: ${lists[index].createdBy}")
+                                        Text(text = "Last edited: ${lists[index].lastEdited}")
+                                    }
+                                    Column(
+                                        Modifier
+                                            .padding(8.dp),
+                                        horizontalAlignment = Alignment.End
+                                    ) {
+                                        lists[index].listItems.take(2).forEach { item ->
+                                            Text(text = item.itemName, color = Color.Gray)
+                                        }
+                                        Text(text = "...", color = Color.Gray)
+                                    }
                                 }
-                                Text(text = "...", color = Color.Gray)
-                            }
-                        }
-                        Row(
-                            Modifier
-                                .padding(
-                                    PaddingValues(
-                                        start = 16.dp,
-                                        bottom = 8.dp,
-                                        end = 16.dp,
-                                    )
-                                )
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                        ) {
-                            Column(
-                                Modifier
-                                    .padding(8.dp)
-                            ) {
-                                Button(onClick = { /*TODO*/ }) {
-                                    Text(text = "Edit")
-                                }
-                            }
-                            Column(
-                                Modifier
-                                    .padding(8.dp)
-                            ) {
-                                Button(onClick = { /*TODO*/ }) {
-                                    Text(text = "Start shopping")
+                                Row(
+                                    Modifier
+                                        .padding(
+                                            PaddingValues(
+                                                start = 16.dp,
+                                                bottom = 8.dp,
+                                                end = 16.dp,
+                                            )
+                                        )
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                ) {
+                                    Column(
+                                        Modifier
+                                            .padding(8.dp)
+                                    ) {
+                                        Button(onClick = { /*TODO*/ }) {
+                                            Text(text = "Edit")
+                                        }
+                                    }
+                                    Column(
+                                        Modifier
+                                            .padding(8.dp)
+                                    ) {
+                                        Button(onClick = { /*TODO*/ }) {
+                                            Text(text = "Start shopping")
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                }
+                Button(
+                    onClick = { addGroceryList() },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "ADD NEW LIST")
                 }
             }
         }
--- app/src/main/java/com/example/grosharies/ui/groups/GroupView.kt
+++ app/src/main/java/com/example/grosharies/ui/groups/GroupView.kt
@@ -6,13 +6,13 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.grosharies.ui.groceryList.CreateListOverview
+import com.example.grosharies.ui.groceryList.ListOverview
 
 val GROUP_VIEW_ROUTE: String = "group/view/{groupId}"
 
 @Composable
 fun GroupView(groupId: String?) {
     Surface(color = MaterialTheme.colors.background, modifier = Modifier.padding(16.dp)) {
-        CreateListOverview(groupId)
+        ListOverview(groupId)
     }
 }
