8352a4d3151af2bb53a2e4cf7f743d1012c37674
Mikal van Eijk <mikal.van.eijk@hva.nl>
2021-11-16 12:34:21 +0100

    fixed the styling for the new item and button, only need to get the bottom navigation padding from the group_overview branch

--- app/src/main/java/com/example/grosharies/ui/groceryList/EditList.kt
+++ app/src/main/java/com/example/grosharies/ui/groceryList/EditList.kt
@@ -1,7 +1,9 @@
 package com.example.grosharies.ui.groceryList
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,12 +32,24 @@ fun EditList(GroupId: String? = null, listId: String? = null) {
                     ListItem("Item4", 362, false),
                     ListItem("Item5", 123, false),
                     ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
+                    ListItem("Item6", 33, false),
                 )
             )
         val listItems = groceryList.listItems
         Column(
-            modifier = Modifier.fillMaxHeight(),
-            verticalArrangement = Arrangement.SpaceBetween
+            modifier = Modifier
+                .fillMaxHeight(),
+//                .verticalScroll(ScrollState(0)),
+            verticalArrangement = Arrangement.SpaceBetween,
         ) {
             LazyColumn {
                 items(listItems.size) { index ->
@@ -88,46 +102,48 @@ fun EditList(GroupId: String? = null, listId: String? = null) {
             var newName: String
             var newAmount: Int
 
-            Row(
-                modifier = Modifier
-                    .padding(
-                        PaddingValues(
-                            start = 16.dp,
-                            end = 16.dp,
+            Column() {
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            PaddingValues(
+                                start = 16.dp,
+                                end = 16.dp,
+                            )
                         )
+                        .fillMaxWidth(),
+                ) {
+                    TextField(
+                        value = "",
+                        onValueChange = { newName = it },
+                        label = { Text("New item name:") },
+                        modifier = Modifier
+                            .weight(5f)
+                            .padding(
+                                PaddingValues(
+                                    top = 8.dp,
+                                    bottom = 8.dp,
+                                    end = 4.dp
+                                )
+                            )
                     )
-                    .fillMaxWidth(),
-            ) {
-                TextField(
-                    value = "",
-                    onValueChange = { newName = it },
-                    label = { Text("New item name:") },
-                    modifier = Modifier
-                        .weight(5f)
-                        .padding(
-                            PaddingValues(
-                                top = 8.dp,
-                                bottom = 8.dp,
-                                end = 4.dp
+                    TextField(
+                        value = "",
+                        onValueChange = {
+                            newAmount = it.toInt()
+                        },
+                        label = { Text("Amount:") },
+                        modifier = Modifier
+                            .weight(2f)
+                            .padding(
+                                PaddingValues(
+                                    top = 8.dp,
+                                    bottom = 8.dp,
+                                    start = 4.dp
+                                )
                             )
-                        )
-                )
-                TextField(
-                    value = "",
-                    onValueChange = {
-                        newAmount = it.toInt()
-                    },
-                    label = { Text("Amount:") },
-                    modifier = Modifier
-                        .weight(2f)
-                        .padding(
-                            PaddingValues(
-                                top = 8.dp,
-                                bottom = 8.dp,
-                                start = 4.dp
-                            )
-                        )
-                )
+                    )
+                }
                 Button(
                     onClick = { SaveEdit() }, modifier = Modifier
                         .fillMaxWidth()
@@ -136,9 +152,6 @@ fun EditList(GroupId: String? = null, listId: String? = null) {
                     Text(text = "SAVE")
                 }
             }
-
-
-
         }
     }
 }
