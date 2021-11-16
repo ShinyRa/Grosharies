2e8c49b43c1f60cc375b28eb81fafca138fd6db5
tijs <tijs.de.graaff@hva.nl>
2021-11-16 14:40:05 +0100

    Merge conflict development into Group_Overview

--- app/src/main/java/com/example/grosharies/ui/navigation/DefaultScaffold.kt
+++ app/src/main/java/com/example/grosharies/ui/navigation/DefaultScaffold.kt
@@ -5,18 +5,15 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navigation
 import com.example.grosharies.ui.groceryList.ListOverview
 import com.example.grosharies.ui.Home
-import com.example.grosharies.ui.groceryList.CreateListOverview
 import com.example.grosharies.ui.groups.*
 import com.example.grosharies.ui.groceryList.EditList
-import com.example.grosharies.ui.groups.GroupOverview
 import com.example.grosharies.ui.groups.GroupView
 
 @Composable
@@ -42,12 +39,15 @@ fun DefaultScaffold() {
             Surface(modifier = Modifier.padding(it)) {
                 NavHost(navController = navController, startDestination = Screen.Home.route, route = "root") {
                     composable(route = Screen.Home.route) { Home() }
-                    composable(route = Screen.Groups.route) { GroupOverview(navController) }
-                    composable(route = Screen.GroupDetail.route + "/{groupId}") { entry ->
-                        GroupView(
-                            entry.arguments?.getString("groupId"),
-                            navController = navController
-                        )
+                    navigation(startDestination = Screen.Groups.route, "group") {
+                        composable(route = Screen.Groups.route) {
+                            Overview(navController = navController)
+                        }
+                        composable(route = Screen.GroupNew.route) { New(addGroup = { group: Group -> addGroup(group) }, navController = navController) }
+                        composable(route = Screen.GroupEdit.route) { Edit() }
+                        composable(route = Screen.GroupDetail.route + "/{groupId}") { entry ->
+                            GroupView(groupId = entry.arguments?.getString("groupId"), navController = navController)
+                        }
                     }
                     composable(route = Screen.Lists.route) { ListOverview(navController = navController) }
                     composable(route = Screen.ListEdit.route + "/{groupId}/{listId}") { entry ->
