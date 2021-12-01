1eef6f193ce71b72fa69576810995a59afc8b36a
JarmovanDongen <55130148+JarmovanDongen@users.noreply.github.com>
2021-12-01 05:55:26 -0800

    Added prefab of textfieldinput And connected the value that is being input in name in the database

--- app/src/main/java/com/example/grosharies/ui/common/Texts.kt
+++ app/src/main/java/com/example/grosharies/ui/common/Texts.kt
@@ -1,9 +1,44 @@
 package com.example.grosharies.ui.common
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+
+
+
 
 @Composable
+
 fun DefaultText(text: String) {
     Text(text = "$text")
+}
+
+@Composable
+fun DefaultTextInputField(text: String, modifier: Modifier?, textValue: MutableState<TextFieldValue> ){
+   // var textValue by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+    TextField(
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+        label = { Text(text = text) },
+
+        modifier = Modifier
+              .fillMaxWidth()
+//            .padding(PaddingValues(
+//            top = 8.dp,
+//            bottom = 8.dp,
+//            end = 4.dp))
+              .composed { modifier?: Modifier }
+    )
 }
--- app/src/main/java/com/example/grosharies/ui/groups/NameInput.kt
+++ app/src/main/java/com/example/grosharies/ui/groups/NameInput.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -16,46 +18,59 @@ import com.example.grosharies.ui.common.MainButton
 import com.example.grosharies.ui.navigation.Screen
 import java.util.*
 import com.example.grosharies.data.NameInput.NameInput
+import com.example.grosharies.ui.common.DefaultTextInputField
+import com.example.grosharies.ui.theme.GroshariesTheme
 
 @Composable
 fun NameInput(navController: NavController) {
     val nameValue = remember {
-        mutableStateOf("Name")
+        mutableStateOf(TextFieldValue(""))
     }
     val context = LocalContext.current
     val nameInputViewModel: NameInputViewModel = viewModel(
         factory = NameInputViewModel.NameInputViewModelFactory(context.applicationContext as Application)
 
     )
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(100.dp),
-    ) {
-        InfoText()
+    GroshariesTheme {
+        Column(
+            modifier = Modifier
+                .padding(
+                    PaddingValues(
+                        start = 32.dp,
+                        end = 32.dp,
+                    )
+                )
+                .fillMaxSize()
+        ) {
+            InfoText()
 
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Row {
+                    DefaultTextInputField(
+                        text = "Your Name",  modifier = Modifier
+                            .fillMaxWidth(), textValue = nameValue
+                    )
+                }
+            }
+        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Bottom
         ) {
-            TextField(value = nameValue.value,
-                onValueChange = { nameValue.value = it })
-        }
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Bottom
-    ) {
-        MainButton(text = "SAVE", onClickListener = {
-            navController.navigate(Screen.Groups.route)
-            nameInputViewModel.insertNameInput(
-                NameInput(
-                    name = nameValue.value,
+            MainButton(text = "SAVE", onClickListener = {
+                navController.navigate(Screen.Groups.route)
+                nameInputViewModel.insertNameInput(
+                    NameInput(
+                        name = nameValue.value.text
+                    )
                 )
-            )
-        })
+            })
+        }
     }
 }
 
@@ -63,11 +78,11 @@ fun NameInput(navController: NavController) {
 fun InfoText() {
     androidx.compose.material.Text(
         "Enter your name here",
-        fontSize = 15.sp,
+        fontSize = 20.sp,
         textAlign = TextAlign.Center,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(32.dp)
     )
 }
 
