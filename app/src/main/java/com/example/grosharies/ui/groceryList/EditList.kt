eb96e9cfa61ff749cc0831f980241a53134879da
Hana <hana.el.ada@hva.nl>
2021-12-15 14:57:48 +0100

    edit list name

--- app/src/main/java/com/example/grosharies/ui/groceryList/EditList.kt
+++ app/src/main/java/com/example/grosharies/ui/groceryList/EditList.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,75 +23,35 @@ fun EditList(groupId: String? = null, listId: String? = null, navController: Nav
 
     var newAmount = 0
     var newName = ""
+    var editGroceryname = remember { mutableStateOf(" ")}
 
     fun SaveEdit() {
 
-//        if (newAmount != 0 && newName.isNotEmpty()) {
-//            viewModel.insertGroceryLists()
-//        }
 
         navController.navigate(Screen.GroupDetail.withArgs(groupId.toString()))
         // TODO: send edited values to the overview
     }
 
     GroshariesTheme {
-//        val listItems = ListItemViewModel.get
-//        Column(
-//            modifier = Modifier
-//                .fillMaxHeight(),
-//            verticalArrangement = Arrangement.SpaceBetween,
-//        ) {
-//            LazyColumn {
-//                items(listItems.size) { index ->
-//                    var name = listItems[index].itemName
-//                    var amount = listItems[index].itemAmount
-//                    Row(
-//                        modifier = Modifier
-//                            .padding(
-//                                PaddingValues(
-//                                    start = 16.dp,
-//                                    top = 8.dp,
-//                                    end = 16.dp,
-//                                )
-//                            )
-//                            .fillMaxWidth(),
-//                    ) {
-//                        TextField(
-//                            value = listItems[index].itemName,
-//                            onValueChange = { name = it },
-//                            label = { Text("Item name:") },
-//                            modifier = Modifier
-//                                .weight(5f)
-//                                .padding(
-//                                    PaddingValues(
-//                                        top = 4.dp,
-//                                        bottom = 4.dp,
-//                                        end = 4.dp
-//                                    )
-//                                )
-//                        )
-//                        TextField(
-//                            value = listItems[index].itemAmount.toString(),
-//                            onValueChange = {
-//                                amount = it.toInt()
-//                            },
-//                            label = { Text("Amount:") },
-//                            modifier = Modifier
-//                                .weight(2f)
-//                                .padding(
-//                                    PaddingValues(
-//                                        top = 4.dp,
-//                                        bottom = 4.dp,
-//                                        start = 4.dp
-//                                    )
-//                                )
-//                        )
-//                    }
-//                }
-//            }
         setTitle("Edit List")
 
         Column {
+
+                TextField(
+                    value = editGroceryname.value ,
+                    onValueChange = { editGroceryname.value = it },
+                    label = { Text ( " Edit the name of your list :") },
+                    modifier = Modifier
+                        .weight(5f)
+                        .padding(
+                            PaddingValues(
+                                top = 8.dp,
+                                bottom = 8.dp,
+                                end = 4.dp,
+                            )
+                        )
+                )
+
             Row(
                 modifier = Modifier
                     .padding(
@@ -130,6 +92,7 @@ fun EditList(groupId: String? = null, listId: String? = null, navController: Nav
                             )
                         )
                 )
+
             }
             ClickableText(
                 text = AnnotatedString("Add new row"),
